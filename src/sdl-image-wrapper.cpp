fe2fc79d28cfd98af0e585a35f20a9fdeead1364
Noah Nichols <1101qbtext@gmail.com>
2018-09-28 20:11:09 -0400

    Initial commit

---
+++ src/sdl-image-wrapper.cpp
@@ -0,0 +1,74 @@
+#include "sdl-image-wrapper.hpp"
+
+SDLImageWrapperException::SDLImageWrapperException(std::string const &message): m_message(message)
+{
+
+}
+
+SDLImageWrapper::SDLImageWrapper(): m_renderer(nullptr),
+                              m_image(nullptr),
+                              m_image_location{0, 0, 0, 0}
+{
+
+}
+
+SDLImageWrapper::SDLImageWrapper(SDL_Renderer *renderer, std::string const &image, int x, int y): m_renderer(renderer),
+                                                                                                  m_image(nullptr),
+                                                                                                  m_image_location{0, 0, 0, 0}
+{
+    SDL_Surface *image_surface = IMG_Load(image.c_str());
+
+    if(!image_surface)
+    {
+    }
+
+    m_image = SDL_CreateTextureFromSurface(renderer, image_surface);
+    SDL_FreeSurface(image_surface);
+
+    m_image_location.x = x;
+    m_image_location.y = y;
+
+    SDL_QueryTexture(m_image, nullptr, nullptr, &m_image_location.w, &m_image_location.h);
+}
+
+SDLImageWrapper::~SDLImageWrapper()
+{    
+    if(m_image)
+        SDL_DestroyTexture(m_image);
+}
+
+void SDLImageWrapper::set_position(int new_x, int new_y)
+{
+    m_image_location.x = new_x;
+    m_image_location.y = new_y;
+}
+
+void SDLImageWrapper::move(int add_x, int add_y)
+{
+    m_image_location.x += add_x;
+    m_image_location.y += add_y;
+}
+
+void SDLImageWrapper::open_image(SDL_Renderer *renderer, const std::string &image_name, int x, int y)
+{
+    SDL_Surface *image_surface = IMG_Load(image_name.c_str());
+
+    if(!image_surface)
+    {
+    }
+
+    m_image = SDL_CreateTextureFromSurface(renderer, image_surface);
+    SDL_FreeSurface(image_surface);
+
+    m_image_location.x = x;
+    m_image_location.y = y;
+
+    m_renderer = renderer;
+
+    SDL_QueryTexture(m_image, nullptr, nullptr, &m_image_location.w, &m_image_location.h);
+}
+
+void SDLImageWrapper::render_image()
+{  
+    SDL_RenderCopy(m_renderer, m_image, nullptr, &m_image_location);
+}
---
+++ src/sdl-image-wrapper.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <SDL2/SDL_image.h>
+#include <string>
+#include <exception>
+
+class SDLImageWrapperException: std::exception
+{
+public:
+    SDLImageWrapperException(std::string const &message);
+    char const *what() { return m_message.c_str(); }
+
+private:
+    std::string m_message;
+};
+
+class SDLImageWrapper
+{
+public:
+    SDLImageWrapper();
+    SDLImageWrapper(SDL_Renderer *renderer, std::string const &image_name, int x, int y);
+    ~SDLImageWrapper();
+
+    void open_image(SDL_Renderer *renderer, const std::string &image_name, int x, int y);
+
+    void set_renderer(SDL_Renderer *renderer) { m_renderer = renderer; }
+
+    SDL_Renderer const *get_renderer() const { return m_renderer; } 
+    SDL_Renderer *get_renderer() { return m_renderer; }
+
+    void set_position(int new_x, int new_y);
+    void move(int add_x, int add_y);
+
+    int &get_x() { return m_image_location.x; }
+    int const &get_x() const { return m_image_location.x; }
+
+    int &get_y() { return m_image_location.y; }
+    int const &get_y() const { return m_image_location.y; }
+
+    int &get_width() { return m_image_location.w; }
+    int const &get_width() const { return m_image_location.w; }
+
+    int &get_height() { return m_image_location.h; }
+    int const &get_height() const { return m_image_location.h; }
+
+    void render_image();
+
+private:
+    SDL_Renderer *m_renderer;
+    SDL_Texture *m_image;
+    SDL_Rect m_image_location;
+};
